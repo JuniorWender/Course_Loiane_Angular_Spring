c06a0208e1a3fd79400ff5286b4752408744aecf
wender Junior <wender.junior@etaure.com.br>
2022-12-13 10:12:29 -0300

    add loading icon with the table and error mensage

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,10 @@
-import { CoursesService } from './../services/courses.service';
-import { Course } from '../model/course';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+
+import { Course } from '../model/course';
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -9,19 +13,31 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
 
-  courses: Course[] = [];
+  courses$: Observable<Course[]>;
 
-  displayedColumns = [ 'name','category' ];
+  displayedColumns = [ 'Name','Category' ];
 
   // CoursesService: CoursesService;
 
-  constructor(private CoursesService: CoursesService) {
+  constructor(private CoursesService: CoursesService,public dialog: MatDialog) {
     // this.courses = [];
     // this.CoursesService = new CoursesService();
+    this.courses$ = this.CoursesService.list().pipe(
+      catchError(error => {
+        this.onError('Erro ao tentar carregar cursos');
+        return of([])
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
   ngOnInit(): void{
-    this.courses = this.CoursesService.list();
+
   }
 
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs';
 
 import { Course } from '../model/course';
 
@@ -12,7 +13,10 @@ export class CoursesService {
 
   constructor(private httpClient: HttpClient) {  }
 
-  list() : Course[] {
-    return this.httpClient.get('');
+  list() {
+    return this.httpClient.get< Course[] >(this.API).pipe(
+      first(),
+      tap(courses => console.log(courses))
+    );
   }
 }
